dd9fd49f282255a4a42257c0dfd0b23cc0d53003
ianframe <ian.frame@hies.org>
2016-10-25 12:17:51 -0400

    add/lose 5 points buttons

--- client/board.js
+++ client/board.js
@@ -25,30 +25,21 @@ Template.scoreboard.helpers({
 });
 
 Template.scoreboard.events({
-  'click .houseName' : function()
+  'click .houseName' : function(event)
   {
     let houseId = this._id;
     Session.set('selectedHouse', houseId);
   },
-  'click .btn-gryffindor': function(event)
+  'click #add5Button' : function(event)
   {
-  	let currentHouseId = Houses.findOne({name : "Gryffindor"})._id;
-    Houses.update({_id : currentHouseId}, {$inc : {points : 5}});
+    let currentHouseId = Session.get('selectedHouse');
+    Houses.update({_id : currentHouseId}, {$inc: {points : 5}});
+    
   },
-  'click .btn-ravenclaw': function(event)
+  'click #lose5Button' : function(event)
   {
-  	let currentHouseId = Houses.findOne({name : "Ravenclaw"})._id;
-    Houses.update({_id : currentHouseId}, {$inc : {points : 5}});
-  },
-  'click .btn-hufflepuff': function(event)
-  {
-  	let currentHouseId = Houses.findOne({name : "Hufflepuff"})._id;
-    Houses.update({_id : currentHouseId}, {$inc : {points : 5}});
-  },
-  'click .btn-slytherin': function(event)
-  {
-  	let currentHouseId = Houses.findOne({name : "Slytherin"})._id;
-    Houses.update({_id : currentHouseId}, {$inc : {points : 5}});
+    let currentHouseId = Session.get('selectedHouse');
+    Houses.update({_id : currentHouseId}, {$inc: {points : -5}});
   }
 });
 
